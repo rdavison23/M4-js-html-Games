28297947d340da16e22211c8338e95cc2a050d71
Your Name <your-email@example.com>
2025-11-26 18:50:56 -0800

    added image display code

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ export default function App() {
   return (
     <div className="App">
       <h1>Hello from Techtonica</h1>
+      {imageFace()}
     </div>
   );
+}
+
+const face = require('./img/s801_a.jpg');
+export function imageFace() {
+  return <img src={face} />;
 }
