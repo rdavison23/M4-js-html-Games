0a88892ab7585c23c871380ccc6726481edc9d70
Your Name <your-email@example.com>
2025-11-28 22:35:00 -0800

    renamed h1 to Heads or Tails

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello from Techtonica</h1>
+      <h1>Heads or Tails</h1>
       {imageFace()}
       {imageTail()}
     </div>
